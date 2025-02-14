ca6b90def3c5bce06ad0a81845cb2c8889d54444
Vlad Karmazin <v.karmazin@stroki.dev>
2025-02-15 04:34:00 +0500

    MVC

--- manager-app/src/main/java/ru/karmazin/manager/ManagerAppApplication.java
+++ manager-app/src/main/java/ru/karmazin/manager/ManagerAppApplication.java
@@ -1,4 +1,4 @@
-package ru.karmazin.managerapp;
+package ru.karmazin.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- manager-app/src/test/java/ru/karmazin/manager/ManagerAppApplicationTests.java
+++ manager-app/src/test/java/ru/karmazin/manager/ManagerAppApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.karmazin.managerapp;
+package ru.karmazin.manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
